eb6cea3806258ea76b36e50eea65be6bddf1f298
jiazhuolaw000919-hub <jiazhuolaw000919@gmail.com>
2026-09-03 13:57:47 +0800

    Update loader.js

--- js/loader.js
+++ js/loader.js
@@ -201,7 +201,10 @@ var STAGES = {
         "calendar/CalendarRenderer.js",
         "calendar.js",
         "themeEngine.js",
-        "settings.js"
+        "settings.js",
+        "academy/knowledgeCapture.js",
+        "academy/notes.js",
+        "academy/secondBrain.js",
     ],
     ux: [
         "experienceComposer.js",
